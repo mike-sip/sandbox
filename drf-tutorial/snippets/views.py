95ae1b8408fc4bd5ab097f667b8d55649554a171
mike-sip <mikesip@hotmail.fr>
2025-09-22 18:29:35 +0200

    Add permissions to `Snippet` views and enable login URLs
    - Implement `IsOwnerOrReadOnly` permission to restrict edit access to snippet owners.
    - Update `SnippetDetail` and `SnippetList` views to include `permissions.IsAuthenticatedOrReadOnly`.
    - Extend URLs to include login routes for API authentication.

--- drf-tutorial/snippets/views.py
+++ drf-tutorial/snippets/views.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -16,6 +18,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
