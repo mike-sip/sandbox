4c85d511a6917aee2a85ed993fd7f9ddc84ca5cd
mike-sip <mikesip@hotmail.fr>
2025-10-06 16:22:32 +0200

    Replace greeting utility usage with basic arithmetic operations in `main.go`.

--- golang/main.go
+++ golang/main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
-	"golang/utils" // Import d’un package local
 )
 
 func main() {
-	message := utils.GetGreeting("Go")
-	fmt.Println(message)
+	var a, b int
+	fmt.Print("Entrez le premier nombre : ")
+	fmt.Scanln(&a)
+	fmt.Print("Entrez le deuxième nombre : ")
+	fmt.Scanln(&b)
+
+	fmt.Println("Addition :", a+b)
+	fmt.Println("Soustraction :", a-b)
+	fmt.Println("Multiplication :", a*b)
+	if b != 0 {
+		fmt.Println("Division :", a/b)
+	} else {
+		fmt.Println("Division par zéro impossible")
+	}
+	fmt.Println("Modulo :", a%b)
 }
